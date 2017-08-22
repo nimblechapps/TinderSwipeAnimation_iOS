58448f0b98d81c62c9e76bcec54465b4be4bff2b
Nimblechapps <contact@nimblechapps.com>
2017-08-22 11:25:16 +0530

    Commit 1

--- TinderSwipeGesture/AppDelegate.swift
+++ TinderSwipeGesture/AppDelegate.swift
@@ -10,9 +10,10 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    // commit 1
 
     var window: UIWindow?
-
+   
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
